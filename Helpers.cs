5089a727bb7558e23b61c609d3b1e87ed28c0e59
Tautvydas Milciunas <tmilciunas@gmail.com>
2017-05-21 23:15:36 +0300

    Implemented MD5 algorythm

---
+++ Helpers.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace MD5
+{
+    public static class Helpers
+    {
+        //Rodyti pasirinkimu meniu 
+        public static int DisplayMenu()
+        {
+            Console.WriteLine("Pasirinkite sifravimo algoritma ir spauskite ENTER: ");
+            Console.WriteLine();
+            Console.WriteLine("1. MD5");
+            Console.WriteLine("2. Exit");
+            Console.WriteLine();
+            Console.Write("Pasirinkimas: ");
+            var result = Console.ReadLine();
+
+            if (result == "1")
+            {
+                return Convert.ToInt32(result);
+            }
+            if (result == "2")
+            {
+                return 2;
+            }
+
+            return 0;
+        }
+
+        //Bitu israsymas is nuskaityto failo y string 
+        public static void PrintByteArray(byte[] bytes)
+        {
+            var sb = new StringBuilder("Byte array from file: [] { ");
+            foreach (var b in bytes)
+            {
+                sb.Append(b + ", ");
+            }
+            sb.Append("}");
+            Console.WriteLine(sb.ToString());
+        }
+
+        //Besikraunantis uzrasas
+        public static void Loading()
+        {
+            Console.Write("Loading");
+            for (int i = 0; i < 4; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write(".");
+            }
+        }
+
+        //Programos uzdarymas su delay ir animacija
+        public static void DelayedShutdown()
+        {
+            Console.WriteLine();
+            Console.Write("Shutting down");
+            for (int i = 0; i < 4; i++)
+            {
+                Thread.Sleep(600);
+                Console.Write(".");
+            }
+            Environment.Exit(0);
+        }
+
+        //Programos uzdarymas
+        public static void Shutdown()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Shutting down...");
+            Environment.Exit(0);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace MD5
 {
     class Program
     {
+        private static byte[] _byteArray = null;
+        private static OpenFileDialog _fileDialog;
+
         [STAThread]
         static void Main()
         {
@@ -16,112 +19,51 @@ namespace MD5
 
             try
             {
-                OpenFileDialog fileDialog = new OpenFileDialog();
-                fileDialog.ShowDialog();
-                Console.WriteLine("Selected file: " + fileDialog.FileName);
+                _fileDialog = new OpenFileDialog();
+                _fileDialog.ShowDialog();
+                Console.WriteLine("Selected file: " + _fileDialog.SafeFileName);
                 Console.WriteLine();
 
-                byte[] byteArray = File.ReadAllBytes(fileDialog.FileName);
+                _byteArray = File.ReadAllBytes(_fileDialog.FileName);
 
-                PrintByteArray(byteArray);
+                Helpers.PrintByteArray(_byteArray);
                 Console.WriteLine();
             }
             catch (Exception e)
             {
                 Console.WriteLine("Huston, we have a problem..." + "\n" + e.Message);
-                DelayedShutdown();
+                Helpers.DelayedShutdown();
             }
 
             int userInput = 0;
             while (userInput != 2)
             {
-                userInput = DisplayMenu();
+                userInput = Helpers.DisplayMenu();
 
                 if (userInput == 0)
                 {
                     Console.WriteLine("-----------------------");
-                    Console.WriteLine("Negalimas pasirinkimas!");
+                    Console.WriteLine("Wrong choice!");
                     Console.WriteLine("-----------------------");
                 }
                 else if (userInput == 1)
                 {
-                    Console.WriteLine("EINAM Y MD5!");
+                    Console.WriteLine();
+                    Console.WriteLine("Intense calculations...");
+                    Console.WriteLine();
+
+                    MD5 md5 = new MD5();
+
+                    md5.ValueAsByte = _byteArray;
+                    Console.WriteLine("MD5 hash: " + md5.FingerPrint);
+
+                    //Console.WriteLine(Md5.OutString(_byteArray.ToString()));
+                    Console.ReadLine();
                     break;
                 }
             }
 
-            
-
-            DelayedShutdown();
-        }
-
-        
-
-        //Rodyti pasirinkimu meniu 
-        private static int DisplayMenu()
-        {
-            Console.WriteLine("Pasirinkite sifravimo algoritma ir spauskite ENTER: ");
-            Console.WriteLine();
-            Console.WriteLine("1. MD5");
-            Console.WriteLine("2. Exit");
-            Console.WriteLine();
-            Console.Write("Pasirinkimas: ");
-            var result = Console.ReadLine();
-
-            if (result == "1")
-            {
-                return Convert.ToInt32(result);
-            }
-            if (result == "2")
-            {
-                return 2;
-            }
-
-            return 0;
-        }
-
-        //Bitu israsymas is nuskaityto failo y string 
-        private static void PrintByteArray(byte[] bytes)
-        {
-            var sb = new StringBuilder("Byte array from file: [] { ");
-            foreach (var b in bytes)
-            {
-                sb.Append(b + ", ");
-            }
-            sb.Append("}");
-            Console.WriteLine(sb.ToString());
-        }
-
-        //Besikraunantis uzrasas
-        private static void Loading()
-        {
-            Console.Write("Loading");
-            for (int i = 0; i < 4; i++)
-            {
-                Thread.Sleep(1000);
-                Console.Write(".");
-            }
-        }
-
-        //Programos uzdarymas
-        static void DelayedShutdown()
-        {
-            Console.WriteLine();
-            Console.Write("Shutting down");
-            for (int i = 0; i < 4; i++)
-            {
-                Thread.Sleep(600);
-                Console.Write(".");
-            }
-            Environment.Exit(0);
-        }
-
-        //Programos uzdarymas
-        static void Shutdown()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Shutting down...");
-            Environment.Exit(0);
+            Helpers.DelayedShutdown();
         }
     }
 }
